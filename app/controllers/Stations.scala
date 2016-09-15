e21097d2c6731d489f1e68715a5ecf44e2e8c909
ponkotuy <web@ponkotuy.com>
2016-09-15 19:01:05 +0900

    Fix StationRank Serializer

--- app/controllers/Stations.scala
+++ app/controllers/Stations.scala
@@ -2,13 +2,13 @@ package controllers
 
 import com.github.tototoshi.play2.json4s.Json4s
 import com.google.inject.Inject
-import models.{LineStation, Station}
+import models.{LineStation, Station, StationRankSerializer}
 import org.json4s._
 import play.api.mvc.{Action, Controller}
 
 class Stations @Inject()(json4s: Json4s) extends Controller {
   import json4s._
-  implicit val formats = DefaultFormats
+  implicit val formats = DefaultFormats + new StationRankSerializer
 
   def list() = Action {
     Ok(Extraction.decompose(Station.findAll(Seq(Station.column.id))))
