0275ddc012444bb9219c5325dd380776982e3f2b
ponkotuy <web@ponkotuy.com>
2016-09-20 01:16:11 +0900

    Create game view

--- app/controllers/Games.scala
+++ app/controllers/Games.scala
@@ -6,12 +6,21 @@ import com.github.tototoshi.play2.json4s.Json4s
 import com.google.inject.Inject
 import jp.t2v.lab.play2.auth.AuthElement
 import models.{Game, GameProgress, Mission}
+import org.json4s.{DefaultFormats, Extraction}
 import play.api.mvc.Controller
 import scalikejdbc._
 import utils.MissionTime
 
 class Games @Inject()(json4s: Json4s) extends Controller with AuthElement with AuthConfigImpl {
   import Responses._
+  import json4s._
+
+  implicit val formats = DefaultFormats
+
+  def list() = StackAction(AuthorityKey -> NormalUser) { implicit req =>
+    val games = Game.findAllBy(sqls.eq(Game.column.accountId, loggedIn.id))
+    Ok(Extraction.decompose(games))
+  }
 
   def create(missionId: Long) = StackAction(AuthorityKey -> NormalUser) { implicit req =>
     DB localTx { implicit session =>
--- view/coffee/game/missions.coffee
+++ view/coffee/game/missions.coffee
@@ -3,12 +3,21 @@ $(document).ready ->
     el: '#missions'
     data:
       missions: []
+      games: []
     methods:
       getMissions: ->
         API.getJSON '/api/missions', (json) =>
           @missions = json
+          @getGames()
+      getGames: ->
+        API.getJSON '/api/games', (games) =>
+          @missions.forEach (mission) =>
+            game = _.find games, (g) -> g.missionId == mission.id
+            Vue.set(mission, 'game', game)
       start: (mission) ->
         API.post "/api/game/#{mission.id}", {}, ->
-          location.reload(false)
+          @continue(mission)
+      continue: (mission) ->
+        location.href = "/game/game.html?mission=#{mission.id}"
     ready: ->
       @getMissions()
