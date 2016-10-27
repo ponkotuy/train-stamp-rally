3306688ea92e46484db76f1b55736a38936d66f0
ponkotuy <web@ponkotuy.com>
2016-10-27 21:26:48 +0900

    Create StationStopValidator

--- app/controllers/Validators.scala
+++ app/controllers/Validators.scala
@@ -5,12 +5,12 @@ import authes.Role.Administrator
 import com.github.tototoshi.play2.json4s.Json4s
 import com.google.inject.Inject
 import jp.t2v.lab.play2.auth.AuthElement
-import models.Diagram
+import models.{Diagram, LineStation}
 import org.json4s.{DefaultFormats, Extraction}
 import play.api.mvc.Controller
 import responses.DiagramResponse
-import scalikejdbc.{AutoSession, DBSession}
-import validator.{ErrorSerializer, DiagramValidator}
+import scalikejdbc.AutoSession
+import validator.{DiagramValidator, ErrorSerializer, StationStopValidator}
 
 class Validators @Inject()(json4s: Json4s) extends Controller with AuthElement with AuthConfigImpl {
   import json4s._
@@ -18,13 +18,14 @@ class Validators @Inject()(json4s: Json4s) extends Controller with AuthElement w
   implicit val format = DefaultFormats + ErrorSerializer
 
   def list() = StackAction(AuthorityKey -> Administrator) { implicit req =>
-    val errors = allDiagrams()(AutoSession).flatMap(DiagramValidator.validate)
+    import Diagram.{stopStationRef, trainRef}
+    import LineStation.{lineRef, stationRef}
+    val diagrams = Diagram.joins(trainRef, stopStationRef).findAll()(AutoSession)
+    val diagramsWith = diagrams.map(DiagramResponse.fromDiagram)
+    val stations = LineStation.joins(lineRef, stationRef).findAll()
+    val errors =
+      diagramsWith.flatMap(DiagramValidator.validate) ++
+          new StationStopValidator(diagrams).validate(stations)
     Ok(Extraction.decompose(errors))
-  }
-
-  private[this] def allDiagrams()(implicit session: DBSession) = {
-    import Diagram.{trainRef, stopStationRef}
-    val diagrams = Diagram.joins(trainRef, stopStationRef).findAll()
-    diagrams.map(DiagramResponse.fromDiagram)
   }
 }
---
+++ app/validator/StationStopValidator.scala
@@ -0,0 +1,29 @@
+package validator
+
+import models.{Diagram, LineStation}
+
+import scala.collection.breakOut
+
+// diagrams: 全diagramが必要. stop_stationがjoinされている必要がある
+class StationStopValidator(diagrams: Seq[Diagram]) {
+  import StationStopValidator._
+
+  def validate(stations: Seq[LineStation]): Seq[Error] = {
+    val stationIds: Set[Long] =
+      diagrams.flatMap(_.stops.map(_.lineStationId))(breakOut)
+    stations.flatMap { st =>
+      if(stationIds.contains(st.id)) None
+      else {
+        Some(new UndefinedTrainError(st))
+      }
+    }
+  }
+}
+
+object StationStopValidator {
+  class UndefinedTrainError(station: LineStation) extends Error {
+    override def message: String = s"Undefined arrived train at ${lineName}:${stationName}"
+    def lineName = station.line.map(_.name).getOrElse(s"(lineId = ${station.lineId})")
+    def stationName = station.station.map(_.name).getOrElse(s"(stationId = ${station.stationId})")
+  }
+}
