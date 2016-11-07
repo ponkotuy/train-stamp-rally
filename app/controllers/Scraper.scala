62b8e49f0ada8b0608b3bf5701aa8cda11a73130
ponkotuy <web@ponkotuy.com>
2016-11-08 01:35:07 +0900

    Add station scraper

--- app/controllers/Scraper.scala
+++ app/controllers/Scraper.scala
@@ -7,28 +7,36 @@ import com.google.inject.Inject
 import jp.t2v.lab.play2.auth.AuthElement
 import net.liftweb.util.Html5
 import org.json4s.{DefaultFormats, Extraction}
-import play.api.mvc.Controller
-import scrape.model.{TimeTablePage, TrainPage}
+import play.api.mvc.{Action, Controller}
+import scrape.model.{StationPage, TrainPage}
 
 import scala.io.Codec
 import scala.xml._
 
 class Scraper @Inject()(json4s: Json4s) extends Controller with AuthElement with AuthConfigImpl {
   import Responses._
+  import Scraper._
   import json4s._
 
   implicit val formats = DefaultFormats
 
-  def timeTable(url: String) = StackAction(AuthorityKey -> Administrator) { implicit req =>
+  def station(lineId: String, pageName: String) = Action { implicit req =>
+    val url = s"${Host}/newdata/ekijikoku/${lineId}/${pageName}.htm"
     val xml = loadXML(url)
-    TimeTablePage.fromXML(xml)
+    StationPage.fromXml(xml) match {
+      case Left(str) => notFound(str)
+      case Right(station) =>
+        Ok(Extraction.decompose(station.trains.map(_.replaceAbbr(station.abbr))))
+    }
+  }
+
+  def timeTable(lineId: String, pageName: String) = StackAction(AuthorityKey -> Administrator) { implicit req =>
     ???
   }
 
   def train(lineId: String, trainId: String) = StackAction(AuthorityKey -> Administrator) { implicit req =>
-    val host = "http://www.ekikara.jp"
     val address = s"/newdata/detail/${lineId}/${trainId}.htm"
-    TrainPage.fromXML(loadXML(s"${host}${address}"), address) match {
+    TrainPage.fromXML(loadXML(s"${Host}${address}"), address) match {
       case Left(str) => notFound(str)
       case Right(train) => Ok(Extraction.decompose(train))
     }
@@ -38,4 +46,8 @@ class Scraper @Inject()(json4s: Json4s) extends Controller with AuthElement with
     val html = scala.io.Source.fromURL(url)(Codec("Shift_JIS")).mkString
     Html5.parse(html) openOr NodeSeq.Empty
   }
+}
+
+object Scraper {
+  val Host = "http://www.ekikara.jp"
 }
---
+++ app/scrape/model/StationPage.scala
@@ -0,0 +1,83 @@
+package scrape.model
+
+import scala.collection.breakOut
+import scala.xml.{Node, NodeSeq, Text}
+
+case class StationPage(trains: Seq[StationTrain], abbr: Abbr)
+
+object StationPage {
+  import utils.ParseHelper._
+
+  def fromXml(xml: NodeSeq): Either[String, StationPage] = {
+    import scala.language.implicitConversions
+    implicit def rightBias[A, B](e: Either[A, B]) = e.right
+    for {
+      table <- (xml \\ "table" find (_ \ "@cellpadding" contains Text("8"))).toRight("table")
+      abbrTable <- (xml \\ "table" find (_ \ "@cellpadding" contains Text("1"))).toRight("abbrTable")
+      abbr <- parseAbbr(abbrTable).toRight("abbr")
+    } yield {
+      val trs = (table \ "tbody" \ "tr").tail
+      val trains = trs.flatMap(parseHour)
+      StationPage(trains, abbr)
+    }
+  }
+
+  private def parseHour(tr: Node): Seq[StationTrain] = {
+    val result = for {
+      hourTd <- tr \ "td" find (_ \ "@class" contains Text("lowBg06"))
+      trainsTd <- tr \ "td" find (_ \ "@class" contains Text("lowBg12"))
+    } yield {
+      val hour = (hourTd \ "span" \ "span").text.toInt
+      val minutesTrains = (trainsTd \\ "td" filter (_ \ "@id" nonEmpty)).flatMap(parseTrain)
+      minutesTrains.map(_.withHour(hour))
+    }
+    result.getOrElse(Nil)
+  }
+
+  val TrainRegex = """\[(.+)\](.+)""".r
+  private def parseTrain(td: Node): Seq[MinutesTrain] = {
+    val result = for {
+      tStr <- (td \ "span" \ "span").headOption
+      minutes <- (td \\ "a").headOption
+    } yield {
+      val add = (td \\ "span" find (_ \ "@class" contains Text("textRed"))).map(_.text)
+      tStr.text match {
+        case TrainRegex(typ, dest) => MinutesTrain(dest, Some(typ), minutes.text.toInt, add)
+        case _ => MinutesTrain(tStr.text, None, minutes.text.toInt, add)
+      }
+    }
+    result.toSeq
+  }
+
+  val AbbrRegex = """(.+)\.\.\.(.+)""".r
+  private def parseAbbr(table: Node): Option[Abbr] = {
+    for {
+      typTr <- (table \\ "tr").headOption
+      typTd <- (typTr \\ "td").lift(2)
+      destTr <- (table \\ "tr").lift(1)
+      destTd <- (destTr \\ "td").lift(2)
+    } yield Abbr(parseAbbrElem(typTd), parseAbbrElem(destTd))
+  }
+
+  private def parseAbbrElem(td: Node): Map[String, String] = {
+    val text = norm(td.text).replace("[", "").replace("]", "")
+    text.split(',').map(_.trim).flatMap {
+      case AbbrRegex(k, v) => Some(k -> v)
+      case _ => None
+    }(breakOut)
+  }
+
+  private case class MinutesTrain(dest: String, typ: Option[String], minutes: Int, add: Option[String]) {
+    def withHour(hour: Int): StationTrain =
+      StationTrain(dest, typ, Stop.Time(hour, minutes), add)
+  }
+}
+
+case class StationTrain(dest: String, typ: Option[String], time: Stop.Time, add: Option[String]) {
+  def replaceAbbr(abbr: Abbr) = copy(
+    dest = abbr.dest.getOrElse(dest, dest),
+    typ = typ.map { t => abbr.typ.getOrElse(t, t) }
+  )
+}
+
+case class Abbr(typ: Map[String, String], dest: Map[String, String])
