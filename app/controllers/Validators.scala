adee2727cbc5e2c4a6cdb22b635cce2a5d9afde4
ponkotuy <web@ponkotuy.com>
2016-11-13 22:28:39 +0900

    Speedup validator 2

--- app/controllers/Validators.scala
+++ app/controllers/Validators.scala
@@ -5,10 +5,9 @@ import authes.Role.Administrator
 import com.github.tototoshi.play2.json4s.Json4s
 import com.google.inject.Inject
 import jp.t2v.lab.play2.auth.AuthElement
-import models.{Diagram, LineStation, Train}
+import models.{Diagram, LineStation, StopStation, Train}
 import org.json4s.{DefaultFormats, Extraction}
 import play.api.mvc.Controller
-import responses.DiagramResponse
 import scalikejdbc.AutoSession
 import utils.MethodProfiler
 import validator.{DiagramValidator, ErrorSerializer, LackTrainValidator, StationStopValidator}
@@ -19,24 +18,24 @@ class Validators @Inject()(json4s: Json4s) extends Controller with AuthElement w
   implicit val format = DefaultFormats + ErrorSerializer
 
   def list() = StackAction(AuthorityKey -> Administrator) { implicit req =>
-    import Diagram.stopStationRef
     import LineStation.{lineRef, stationRef}
-    val profiler = MethodProfiler.apply()
+    val profiler = MethodProfiler.nop
     val errors = profiler("all") {
       val diagrams = profiler("diagrams") {
-        Diagram.joins(stopStationRef).findAll()(AutoSession)
+        Diagram.findAll()(AutoSession)
+      }
+      val stops = profiler("stops") {
+        StopStation.findAll()
       }
       val trains = profiler("trains") {
         Train.allDiagramIds()(AutoSession).toSet
-      }
-      val diagramsWith = profiler("diagramsResponses") {
-        diagrams.map(DiagramResponse.fromDiagram)
       }
       val stations = profiler("lineStations") {
         LineStation.joins(lineRef, stationRef).findAll()
       }
       profiler("diagramsValidator") {
-        diagramsWith.flatMap(DiagramValidator.validate)
+        val validator = new DiagramValidator(stops, stations)
+        diagrams.flatMap(validator.validate)
       } ++ profiler("stationStopValidator") {
         new StationStopValidator(diagrams).validate(stations)
       } ++ profiler("lackTrainValidator") {
--- app/validator/DiagramValidator.scala
+++ app/validator/DiagramValidator.scala
@@ -1,17 +1,21 @@
 package validator
 
-import responses.{DiagramResponse, DiagramStationResponse}
+import models.{Diagram, LineStation, StopStation}
 
-object DiagramValidator {
-  def validate(diagram: DiagramResponse): Seq[Error] = {
-    if(diagram.stops.size < 2) { return List(new LackStopsError(diagram)) }
-    diagram.stops.sliding(2).flatMap { case Seq(x, y) =>
-      if(x.line.id != y.line.id && x.station.id != y.station.id) Some(new LineConnectionError(diagram, x, y))
+class DiagramValidator (allStops: Seq[StopStation], allLineStations: Seq[LineStation]) {
+  def validate(diagram: Diagram): Seq[Error] = {
+    val stops = allStops.filter(_.diagramId == diagram.id)
+    if(stops.size < 2) { return List(new LackStopsError(diagram)) }
+    stops.sliding(2).flatMap { case Seq(x, y) =>
+      val lineStationX = allLineStations.find(_.id == x.lineStationId).get
+      val lineStationY = allLineStations.find(_.id == y.lineStationId).get
+      if(lineStationX.lineId != lineStationY.lineId &&
+          lineStationX.stationId != lineStationY.stationId) Some(new LineConnectionError(diagram, x, y))
       else None
     }.toSeq
   }
 
-  abstract class DiagramError(diagram: DiagramResponse) extends Error {
+  abstract class DiagramError(diagram: Diagram) extends Error {
     def content: String
     override def message: String =
       s"DiagramId = ${diagram.id}: ${content}"
@@ -20,14 +24,14 @@ object DiagramValidator {
   }
 
   class LineConnectionError(
-      diagram: DiagramResponse,
-      stop1: DiagramStationResponse,
-      stop2: DiagramStationResponse) extends DiagramError(diagram) {
+      diagram: Diagram,
+      stop1: StopStation,
+      stop2: StopStation) extends DiagramError(diagram) {
     override def content: String =
-      s"Not connect from ${stop1.line.name}:${stop1.station.name} to ${stop2.line.name}:${stop2.station.name}"
+      s"Not connect from ${stop1.lineStationId} to ${stop2.lineStationId}"
   }
 
-  class LackStopsError(diagram: DiagramResponse) extends DiagramError(diagram) {
+  class LackStopsError(diagram: Diagram) extends DiagramError(diagram) {
     override def content: String = "Lack of stops"
   }
 }
