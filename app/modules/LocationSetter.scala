37945275ea5e2e228e929a375baa34a22c57c628
ponkotuy <web@ponkotuy.com>
2016-12-18 17:02:52 +0900

    Fix geo query

--- app/modules/LocationSetter.scala
+++ app/modules/LocationSetter.scala
@@ -1,50 +1,18 @@
 package modules
 
 import com.google.inject.{AbstractModule, Inject, Singleton}
+import com.google.maps.model.GeocodingResult
 import models.{LineStation, StationGeo}
 import play.api.Configuration
-import utils.{Config, GoogleMaps}
 import scalikejdbc._
-
-import scala.concurrent.{ExecutionContext, Future}
+import utils.{Config, GoogleMaps}
 
 @Singleton
-class LocationSetter @Inject() (config: Configuration, _p: PlayInitializer, ec: ExecutionContext) {
-  import LocationSetter._
-  import models.DefaultAliases.sg
-  val conf = new Config(config)
-  val maps = new GoogleMaps(conf.googleMapsKey)
-  implicit val _ec: ExecutionContext = ec
-
+class LocationSetter @Inject() (config: Configuration, _p: PlayInitializer) {
   run()
 
   def run(): Unit = {
-    Future {
-      Iterator.from(0).map { idx =>
-        val xs = LineStation.joins(LineStation.stationRef, LineStation.lineRef)
-          .findAllWithLimitOffset(limit = FindCount, offset = idx * FindCount)
-        xs.foreach { x =>
-          val geo = StationGeo.findBy(sqls.eq(sg.stationId, x.stationId))
-          if (geo.isEmpty) {
-            val result = for {
-              station <- x.station
-              line <- x.line
-              geo = maps.geocoding.request(s"${line.name} ${normStationName(station.name)}駅")
-              _ = Thread.sleep(1000L)
-            } yield geo
-            result.foreach { geo =>
-              if(geo.length == 1) {
-                val loc = geo.head.geometry.location
-                val sg = StationGeo(x.stationId, loc.lat, loc.lng)
-                sg.save()(AutoSession)
-              }
-            }
-          }
-        }
-        Thread.sleep(100L)
-        xs.nonEmpty
-      }.takeWhile(identity).foreach(_ => ())
-    }
+    new Thread(new LocationSetterThread(config)).start()
   }
 }
 
@@ -53,6 +21,47 @@ object LocationSetter {
   val ReBranket = """\(.*\)""".r
   def normStationName(str: String) = ReBranket.replaceAllIn(str, "")
 
+  def saveGeo(stationId: Long, geo: GeocodingResult)(implicit session: DBSession): Unit = {
+    val loc = geo.geometry.location
+    val sg = StationGeo(stationId, loc.lat, loc.lng)
+    sg.save()(AutoSession)
+  }
+
+  def geoQuery(maps: GoogleMaps)(q: String): Option[GeocodingResult] = {
+    val xs = maps.geocoding.request(q)
+    if (xs.length == 1) Some(xs.head) else None
+  }
+}
+
+class LocationSetterThread(config: Configuration) extends Runnable {
+  import LocationSetter._
+  val conf = new Config(config)
+  val maps = new GoogleMaps(conf.googleMapsKey)
+
+  override def run(): Unit = {
+    import models.DefaultAliases.sg
+    Stream.from(0).map { idx =>
+      println(idx)
+      val xs = LineStation.joins(LineStation.stationRef, LineStation.lineRef)
+        .findAllWithLimitOffset(limit = FindCount, offset = idx * FindCount)
+      xs.foreach { x =>
+        val geo = StationGeo.findBy(sqls.eq(sg.stationId, x.stationId))
+        if (geo.isEmpty) {
+          for {
+            station <- x.station
+            line <- x.line
+          } {
+            val geo = geoQuery(maps)(s"${line.name} ${normStationName(station.name)}駅")
+              .orElse(geoQuery(maps)(s"${normStationName(station.name)}駅"))
+            Thread.sleep(1000L)
+            geo.foreach(saveGeo(x.stationId, _)(AutoSession))
+          }
+        }
+      }
+      Thread.sleep(100)
+      xs.nonEmpty
+    }.takeWhile(identity).foreach(_ => ())
+  }
 }
 
 class LocationSetterModule extends AbstractModule {
