ae6d5cd485513fdfef61fc78c79fb1c0f83955d2
ponkotuy <web@ponkotuy.com>
2017-11-24 22:31:34 +0900

    Enable ETag to History.list

--- app/controllers/Histories.scala
+++ app/controllers/Histories.scala
@@ -7,20 +7,25 @@ import authes.Role.Administrator
 import com.github.tototoshi.play2.json4s.Json4s
 import models.{Diagram, History}
 import org.json4s.{DefaultFormats, Extraction}
-import play.api.mvc.InjectedController
+import play.api.mvc.{InjectedController, Request, Result}
 import queries.CreateHistory
 import scalikejdbc._
 
 class Histories @Inject() (json4s: Json4s) extends InjectedController with Authenticator {
+  import Histories.withETag
   import controllers.Responses._
   import json4s._
   import json4s.implicits._
 
   implicit val format = DefaultFormats
 
-  def list() = Action {
+  def list() = Action { implicit req =>
     import models.DefaultAliases.h
-    Ok(Extraction.decompose(History.findAllWithLimitOffset(limit = 20, orderings = Seq(h.id.desc))))
+    val lastId = History.lastId()(AutoSession).getOrElse(0L)
+    withETag(lastId.toString) {
+      val histories = History.findAllWithLimitOffset(limit = 20, orderings = Seq(h.id.desc))
+      Ok(Extraction.decompose(histories))
+    }
   }
 
   def create() = Action(json) { implicit req =>
@@ -34,6 +39,20 @@ class Histories @Inject() (json4s: Json4s) extends InjectedController with Authe
           Success
         }
       }
+    }
+  }
+
+}
+
+object Histories {
+  import play.api.http.HeaderNames._
+  import play.api.mvc.Results._
+
+  def withETag(eTag: String)(content: => Result)(implicit req: Request[Any]): Result = {
+    if (req.headers.get(IF_NONE_MATCH).contains(eTag)) {
+      NotModified
+    } else {
+      content.withHeaders(ETAG -> eTag)
     }
   }
 }
--- app/models/History.scala
+++ app/models/History.scala
@@ -1,6 +1,6 @@
 package models
 
-import scalikejdbc.{DBSession, WrappedResultSet, autoConstruct}
+import scalikejdbc._
 import skinny.orm.{Alias, SkinnyCRUDMapperWithId}
 
 case class History(id: Long, message: String, created: Long) {
@@ -8,6 +8,7 @@ case class History(id: Long, message: String, created: Long) {
 }
 
 object History extends SkinnyCRUDMapperWithId[Long, History] {
+  import DefaultAliases.h
   override val defaultAlias: Alias[History] = createAlias("h")
 
   override def extract(rs: WrappedResultSet, n: scalikejdbc.ResultName[History]): History = autoConstruct(rs, n)
@@ -18,4 +19,8 @@ object History extends SkinnyCRUDMapperWithId[Long, History] {
 
   def save(h: History)(implicit session: DBSession): Long =
     createWithAttributes('message -> h.message, 'created -> h.created)
+
+  def lastId()(implicit session: DBSession): Option[Long] = withSQL {
+    select(h.id).from(History as h).orderBy(h.id.desc).limit(1)
+  }.map(_.long(1)).single().apply()
 }
